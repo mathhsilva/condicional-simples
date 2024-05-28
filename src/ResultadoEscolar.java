8d9d0545e801ddef56fb9ae23ecd37f6b84db245
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-05-28 12:15:50 -0300

    Commit ResultadoEscolar

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-           int nota = 8;
-            if (nota >= 7)
-            System.out.println("Aprovado");
-            else
-            System.out.println("Reprovado");
+           int nota = 4;
+            if (nota >= 7) {
+                System.out.println("Aprovado!");
+            }else if (nota >= 5 && nota < 7){
+            System.out.println("Recuperação!");
+        }else{
+                System.out.println("Reprovado!");
+            }
     }
 }
